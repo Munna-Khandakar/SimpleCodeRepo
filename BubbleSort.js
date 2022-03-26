e1702ea768651a21deb6d336b592258f2e6ff73c
Munna-Khandakar <42739746+Munna-Khandakar@users.noreply.github.com>
2022-03-27 01:12:05 +0600

    Create BubbleSort.js

---
+++ BubbleSort.js
@@ -0,0 +1,18 @@
+const arra = [2, 6, 3, 5, 1, 8, 9];
+
+// time complexity O(n^2)
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 2; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+};
+
+console.log(bubbleSort(arra));
